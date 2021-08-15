ce28c8d3948c251aa286fc1a839a6849d490888c
Savage Sparky <akilan73210@gmail.com>
2021-08-15 17:01:41 +0530

    ham butto bug fixa

--- 5_portfolio/main.js
+++ 5_portfolio/main.js
@@ -1,11 +1,23 @@
 const hamButton = document.getElementById("ham");
 const navLink = document.getElementById('navLinks');
 
+
+
 hamButton.addEventListener('click', () => {
-    if(navLink.style.display == "none"){
+    // if(navLink.style.display == "none"){
+    //     navLink.style.display = "flex";
+    //     console.log("triggered");
+    // }
+    // else {
+    //     navLink.style.display = "none";
+    //     console.log("not triggered");
+    // }
+    if(hamButton.classList.contains("close")){
+        hamButton.classList.remove("close");
         navLink.style.display = "flex";
     }
-    else {
+    else{
+        hamButton.classList.add("close");
         navLink.style.display = "none";
     }
 });
